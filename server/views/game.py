65e38a8ea36dcc8c90ca3ba7556054f32cc8fc93
joycemcliu <7aminafternoon@gmail.com>
2024-10-12 12:58:25 +0800

    Update server implementation

--- server/views/game.py
+++ server/views/game.py
@@ -27,26 +27,35 @@ class NewGameResp(BaseModel):
     id: UUID
     user_id: UUID
     max_rounds: int
-    num_attemps: int
-    score: int
+    num_attempts: int
+    is_end: bool
 
     class Config:
         orm_mode = True  # Allows Pydantic to work with SQLAlchemy models
 
 
-class GuessResp(BaseModel):
-    game_id: UUID
-    user_id: UUID
-    num_attemps: int
+class GuessResp(NewGameResp):
     is_end: bool
-    max_rounds: int
     hint: str
+    answer: str = ""
 
     class Config:
         orm_mode = True
 
 
 words_list = ["hello", "world", "quite", "fancy", "fresh", "panic", "crazy", "buggy"]
+
+
+def check_guess(guess: str, answer: str) -> str:
+    hint = ""
+    for i in range(len(answer)):
+        if answer[i] == guess[i]:
+            hint += Hint.HIT.value
+        elif guess[i] in answer:
+            hint += Hint.PRESENT.value
+        else:
+            hint += Hint.MISS.value
+    return hint
 
 
 @router.get("/new", response_model=NewGameResp)
@@ -66,15 +75,16 @@ async def new_game(user_id: str | None = None, db: AsyncSession = Depends(get_db
 
 @router.get("/submit", response_model=GuessResp)
 async def submit_guess(
-    game_id: str,
+    id: str,
     guess: str,
     db: AsyncSession = Depends(get_db_session),
 ):
-    game = await GameModel.get(db, game_id)
+    guess = guess.lower()
+    game = await GameModel.get(db, id)
     if game is None:
         raise HTTPException(status_code=404, detail="Game not found")
 
-    if game.num_attemps >= game.max_rounds or game.is_end:
+    if game.num_attempts >= game.max_rounds or game.is_end:
         raise HTTPException(status_code=400, detail="Game is over")
 
     if len(guess) != len(game.answer):
@@ -86,32 +96,31 @@ async def submit_guess(
 
     history = GameHistory(game_id=game.id, word=guess, answer=game.answer)
 
-    hint = ""
-    for i in range(len(game.answer)):
-        if game.answer[i] == guess[i]:
-            hint += Hint.HIT.value
-        elif guess[i] in game.answer:
-            hint += Hint.PRESENT.value
-        else:
-            hint += Hint.MISS.value
+    hint = check_guess(guess=guess, answer=game.answer)
 
     history.hit_count = hint.count(Hint.HIT.value)
     history.present_count = hint.count(Hint.PRESENT.value)
     history.miss_count = hint.count(Hint.MISS.value)
 
+    answer = ""
     if hint == Hint.HIT.value * len(game.answer):
         game.is_end = True
+        answer = game.answer
     if not game.is_end:
-        game.num_attemps += 1
+        game.num_attempts += 1
+        if game.num_attempts >= game.max_rounds:
+            game.is_end = True
+            answer = game.answer
 
     await db.commit()
     await history.insert(db)
 
     return GuessResp(
-        game_id=game.id,
+        id=game.id,
         user_id=game.user_id,
-        num_attemps=game.num_attemps,
-        is_end=game.is_end,
         max_rounds=game.max_rounds,
+        num_attempts=game.num_attempts,
+        is_end=game.is_end,
         hint=hint,
+        answer=answer,
     )

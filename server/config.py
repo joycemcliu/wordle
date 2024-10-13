0b4247ea777e10555848075ab016769f1e20fa77
joycemcliu <7aminafternoon@gmail.com>
2024-10-13 11:06:37 +0800

    Set DEFAULT_WORD_LIST as env variable

--- server/config.py
+++ server/config.py
@@ -3,6 +3,19 @@ import os
 ENV = os.environ.get("ENV", "dev")
 DEFAULT_MAX_ATTEMPTS = os.environ.get("MAX_ATTEMPTS", 6)
 DEFAULT_LEN_WORD = os.environ.get("LEN_WORD", 5)
-DEFAULT_WORD_LIST = os.environ.get(
-    "WORD_LIST", "hello,world,quite,fancy,fresh,panic,crazy,buggy,scare"
-).split(",")
+
+DEFAULT_WORD_LIST = os.environ.get("WORD_LIST")
+if DEFAULT_WORD_LIST:
+    DEFAULT_WORD_LIST = DEFAULT_WORD_LIST.split(",")
+else:
+    DEFAULT_WORD_LIST = [
+        "hello",
+        "world",
+        "quite",
+        "fancy",
+        "fresh",
+        "panic",
+        "crazy",
+        "buggy",
+        "scare",
+    ]
